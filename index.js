6dd10c62072a824c02a7199a83cc454a08ebbcf2
Shuddho11288 <76694476+Shuddho11288@users.noreply.github.com>
2021-10-08 16:38:19 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function scrollDown(){
+    var elmnt = document.getElementById("download-element");
+    elmnt.scrollIntoView();
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
